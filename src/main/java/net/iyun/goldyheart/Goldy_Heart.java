8a82759ac10e5d9af25f1923093112df93589033
iYun-main <thezigix299@gmail.com>
2024-07-18 18:32:17 +0200

    Values Changes

--- src/main/java/net/iyun/goldyheart/Goldy_Heart.java
+++ src/main/java/net/iyun/goldyheart/Goldy_Heart.java
@@ -27,31 +27,31 @@ public class Goldy_Heart implements ModInitializer {
 		ModCustomTrades.registerCustomTrades();
 
 
-		CompostingChanceRegistry.INSTANCE.add(ModItems.GRAPESEEDS,0.05f);
-		CompostingChanceRegistry.INSTANCE.add(ModItems.BELLPEPPER,0.2f);
+		CompostingChanceRegistry.INSTANCE.add(ModItems.GRAPESEEDS,0.3f);
+		CompostingChanceRegistry.INSTANCE.add(ModItems.BELLPEPPER,0.1f);
 		CompostingChanceRegistry.INSTANCE.add(ModItems.RICE,0.1f);
 		CompostingChanceRegistry.INSTANCE.add(ModItems.GARLIC,0.1f);
-		CompostingChanceRegistry.INSTANCE.add(ModItems.BELLPEPPERSLICE,0.05f);
-		CompostingChanceRegistry.INSTANCE.add(ModItems.BELLPEPPERSEEDS,0.05f);
-		CompostingChanceRegistry.INSTANCE.add(ModItems.ONION,0.05f);
-		CompostingChanceRegistry.INSTANCE.add(ModItems.ONIONSLICE,0.01f);
-		CompostingChanceRegistry.INSTANCE.add(ModItems.CUTTOMATO,0.01f);
+		CompostingChanceRegistry.INSTANCE.add(ModItems.BELLPEPPERSLICE,0.1f);
+		CompostingChanceRegistry.INSTANCE.add(ModItems.BELLPEPPERSEEDS,0.3f);
+		CompostingChanceRegistry.INSTANCE.add(ModItems.ONION,0.1f);
+		CompostingChanceRegistry.INSTANCE.add(ModItems.ONIONSLICE,0.1f);
+		CompostingChanceRegistry.INSTANCE.add(ModItems.CUTTOMATO,0.1f);
 		CompostingChanceRegistry.INSTANCE.add(ModItems.CUCUMBERSLICE,0.01f);
-		CompostingChanceRegistry.INSTANCE.add(ModItems.PARSLEYSEEDS,0.05f);
-		CompostingChanceRegistry.INSTANCE.add(ModItems.CHILLISEEDS,0.05f);
-		CompostingChanceRegistry.INSTANCE.add(ModItems.TOMATOSEEDS,0.05f);
-		CompostingChanceRegistry.INSTANCE.add(ModItems.LETTUCESEEDS,0.05f);
-		CompostingChanceRegistry.INSTANCE.add(ModItems.CUCUMBERSEEDS,0.05f);
+		CompostingChanceRegistry.INSTANCE.add(ModItems.PARSLEYSEEDS,0.3f);
+		CompostingChanceRegistry.INSTANCE.add(ModItems.CHILLISEEDS,0.3f);
+		CompostingChanceRegistry.INSTANCE.add(ModItems.TOMATOSEEDS,0.3f);
+		CompostingChanceRegistry.INSTANCE.add(ModItems.LETTUCESEEDS,0.3f);
+		CompostingChanceRegistry.INSTANCE.add(ModItems.CUCUMBERSEEDS,0.3f);
 		CompostingChanceRegistry.INSTANCE.add(ModItems.LETTUCE,0.2f);
 		CompostingChanceRegistry.INSTANCE.add(ModItems.TOMATO,0.2f);
 		CompostingChanceRegistry.INSTANCE.add(ModItems.CUCUMBER,0.2f);
-		CompostingChanceRegistry.INSTANCE.add(ModItems.COFFEEBEAN,0.2f);
+		CompostingChanceRegistry.INSTANCE.add(ModItems.COFFEEBEAN,0.15f);
 		CompostingChanceRegistry.INSTANCE.add(ModItems.CHILLI,0.2f);
 		CompostingChanceRegistry.INSTANCE.add(ModItems.PARSLEY,0.2f);
 		CompostingChanceRegistry.INSTANCE.add(ModItems.GRAPES,0.2f);
-		CompostingChanceRegistry.INSTANCE.add(ModItems.STRAWBERRY,0.25f);
-		CompostingChanceRegistry.INSTANCE.add(ModItems.BLUEBERRY,0.25f);
-		CompostingChanceRegistry.INSTANCE.add(ModItems.RHUBARB,0.25f);
+		CompostingChanceRegistry.INSTANCE.add(ModItems.STRAWBERRY,0.05f);
+		CompostingChanceRegistry.INSTANCE.add(ModItems.BLUEBERRY,0.05f);
+		CompostingChanceRegistry.INSTANCE.add(ModItems.RHUBARB,0.05f);
 
 		StrippableBlockRegistry.register(ModBlocks.WEATHERED_LOG, ModBlocks.STRIPPED_WEATHERED_LOG);
 		StrippableBlockRegistry.register(ModBlocks.WEATHERED_WOOD, ModBlocks.STRIPPED_WEATHERED_WOOD);
